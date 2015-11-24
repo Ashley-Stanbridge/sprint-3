d9b8f3d33ba54782d57372448f27568f0150b402
Dev <ashleystanbridge@icloud.com>
2015-11-24 21:02:55 +1300

    freeCodeCamp.js compleat

---
+++ freeCodeCamp.js
@@ -0,0 +1,10 @@
+var joinMe = ["Split","me","into","an","array"];
+
+// Only change code below this line.
+
+var joinedString = joinMe.join(' ');
+console.log();
+
+// Only change code above this line.
+
+(function() {return joinedString;})();
